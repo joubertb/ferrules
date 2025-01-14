883a82bc602567fc4990017e051e8fefb85a3391
aminediro <aminedirhoussi@gmail.com>
2025-01-14 15:26:32 +0100

    build local

--- build.rs
+++ build.rs
@@ -3,9 +3,7 @@ fn main() {
     println!("cargo:rustc-link-arg=-fapple-link-rtlib");
 
     println!("cargo:rustc-link-lib=static=pdfium");
-    println!(
-        "cargo:rustc-link-search=native=/Users/amine/coding/ferrules/libs/pdfium-static-arm64-v6694/lib"
-    );
+    println!("cargo:rustc-link-search=native=./libs/pdfium-static-arm64-v6694/lib");
     println!("cargo:rustc-link-lib=dylib=c++");
     // https://github.com/ajrcarey/pdfium-render/issues/126
     println!("cargo:rustc-link-lib=framework=CoreGraphics");
